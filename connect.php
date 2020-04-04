41a900b3b7d51cee204267c1a6277c83a5720c82
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-04-04 23:08:07 +0300

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+// Username: R9rH7eFGck
+
+// Database name: R9rH7eFGck
+
+// Password: a4YE3DiL5g
+
+// Server: remotemysql.com
+
+// Port: 3306
+
+ $mysqli = new mysqli(
+    'remotemysql.com',
+    'R9rH7eFGck',
+    'a4YE3DiL5g',
+    'R9rH7eFGck'
+);
+?>
