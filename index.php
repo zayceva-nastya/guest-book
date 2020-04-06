b743c79e37dced5674cf69808532a3ca0948416a
zayceva-nastya <59017854+zayceva-nastya@users.noreply.github.com>
2020-04-06 14:13:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="1.css">
 </head>
 
 <body class="px-3">
@@ -22,7 +23,7 @@
   $a = $result->fetch_array();
 
 
-  $count = 20; // колличество записей на странице
+  // $count = 50; // колличество записей на странице
 
   $number = ceil($a[0] / $count); //колличество страниц
 
@@ -35,43 +36,41 @@
 
     echo "Эта таблица содержит\t" . $a[0] . "\tстрок<br>";
 
-    while ($row = $result->fetch_object()) {
-      echo "<div class='d-inline border mr-2 w-10'>" . $row->id .
-        "</div>" . "<b>" . bb_code(smile($row->text)) .
-        " </b><i>" . $row->name . "</i><br>\n";
-    }
-
-    echo "<a href='index.php'>1</a>\t\n";
-    for ($i = 20, $c = 2; $i < $a[0],$c<=$number; $i = $i + $count, $c++) {
-
-      echo "<a href='index.php?p=$i'>$c</a>\t";
-    }
-  } else {
-       $result = $mysqli->query("Select * From gbook Order by id DESC limit $_GET[p], $count");
-
+    echo "<table border='2'>\n";
+    echo "<hr>";
+    echo "<td>№</td>";
+    echo "<td>Name</td>";
+    echo "<td>Text</td>";
     while ($row = $result->fetch_object()) {
 
-      echo "<div class='d-inline border mr-2 w-auto '>" . $row->id .
-        "</div>" . "<b>" . bb_code(smile($row->text)) .
-        " </b><i>\n" . $row->name . "</i><br>\n";
+      echo "<tr>";
+      echo "<td>" .  $row->id . "</td>";
+      echo "<td>" . $row->name . "</td>";
+      echo "<td>" . bb_code(smile($row->text)) . "</td>";
+      echo "</tr>";
     }
+    echo "</table>\n";
+
+    echo "<div class='mt-3'>";
     echo "<a href='index.php'>1</a>\t\n";
-    for ($i = $count, $c = 2; $i < $a[0],$c<=$number; $i = $i + $count, $c++) {
-      echo "<a href='index.php?p=$i'>$c</a>\t\n";
+    for ($i = 20, $c = 2; $i < $a[0], $c <= $number; $i = $i + $count, $c++) {
+
+      echo "<a href='page.php?p=$i&page=$c'>$c</a>\t";
     }
   }
 
-
+  echo "</div>";
   $result->free();
 
   $mysqli->close();
   ?>
   <br>
-  <form action="?" method="post">
+  <form action="gb.php" method="post">
     <textarea name="text"></textarea>
     <input type="text" name="name">
     <input type="submit">
   </form>
+
   <!-- Optional JavaScript -->
   <!-- jQuery first, then Popper.js, then Bootstrap JS -->
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
